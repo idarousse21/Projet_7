c520bbb5d2f7dec3bb59d654b509eb95fd5add83
idarousse <idarousse.i@hotmail.fr>
2022-10-19 11:41:14 +0200

    finalisation du fichier brutforce, modification get_best_combination

--- bruteforce.py
+++ bruteforce.py
@@ -1,7 +1,6 @@
-
-import time
 import csv
 from itertools import combinations
+import timeit
 
 
 def get_actions_from_csv():
@@ -10,8 +9,14 @@ def get_actions_from_csv():
         reader = csv.reader(f, delimiter=";")
         next(reader)
         for row in reader:
-            action = (row[0], float(row[1]), float(row[2]))
-            actions.append(action)
+            if float(row[1]) > 0.0 < float(row[2]):
+                action = (
+                    row[0],
+                    float(row[1]),
+                    float((float(row[1]) * float(row[2])) / 100),
+                )
+                actions.append(action)
+
     return actions
 
 
@@ -23,35 +28,38 @@ def generate_combinations():
 
 
 def get_best_combination():
+    stock_purchase_budget = 500
     best_combination = None
     max_money = 0
-    total_cost_actions = 0
-    total_action_profitability = 0
     for combination in generate_combinations():
+        total_cost_actions = 0
+        total_action_profitability = 0
         for action in combination:
-            total_cost_actions = total_cost_actions + action[1]
-            action_profitability = action[1] * action[2] / 100
-            total_action_profitability = (
-                total_action_profitability + action_profitability
-            )
-            if total_cost_actions <= 500 and action == combination[-1]:
-                total_cost_actions = 0
-                if total_action_profitability > max_money:
-                    best_combination = combination
-                    max_money = total_action_profitability
-                    total_action_profitability = 0
-                else:
-                    total_action_profitability = 0
-            elif total_cost_actions >= 500 and action == combination[-1]:
-                total_cost_actions = 0
-                total_action_profitability = 0
-        pass
-    end_time = time.time() - start_time
-    print(f"La meilleure combinaison est: {best_combination} ")
-    print(f"La rentabilité de cette combinaison est: {round(max_money,2)} €")
-    print(f"le temps d'exectution du code est de: {round(end_time,2)} secondes")
-    return best_combination, max_money
+            total_cost_actions += action[1]
+            if total_cost_actions > stock_purchase_budget:
+                break
+            action_profitability = action[2]
+            total_action_profitability += action_profitability
+        if (
+            total_cost_actions <= stock_purchase_budget
+            and total_action_profitability > max_money
+        ):
+            best_combination = combination
+            max_money = total_action_profitability
+            total_cost_best_action = total_cost_actions
+    return best_combination, total_cost_best_action, max_money
 
 
-start_time = time.time()
-get_best_combination()
+result = get_best_combination()
+numbers = 10
+result_time = timeit.timeit(
+    stmt="get_best_combination()",
+    setup="from __main__ import get_best_combination",
+    number=numbers,
+)
+times = result_time / numbers
+
+print(f"la meilleur combinaison d'action est:\n{result[0]}\n")
+print(f"le prix d'achat de la combinaison est de:\n{result[1]:.2f}€\n")
+print(f"le bénéfice pour l'achat de ces actions est de:\n{result[2]:.2f}€\n")
+print(f"Le temps de lancemant du programme est de:\n{times:.2f} secondes")
